38a60a67804c1ec485d25fe172f87db3e5f4d3c1
Wolfgang Schützenhofer <hallo@wschuetzenhofer.at>
2018-08-09 10:10:14 +0200

    first section styles

--- wp-content/themes/nothing/template-home.php
+++ wp-content/themes/nothing/template-home.php
@@ -7,27 +7,29 @@
 ?>
 <?php get_header(); ?>
 <div class="flex flex-wrap">
-  <div class="w-1/2 bg-brown h-screen">
-    <h1 class="text-white text-7xl">Anna Schukina</h1>
-    <h1 class="text-white text-7xl">Translations</h1>
-    <h2 class="text-grey-darker font-normal">professional. transaltion. service</h2>
-    <div class="flex flex-wrap">
-      <div class="w-1/3">
-        <h3 class="text-white font-normal text-2xl">languages</h3>
-        <p class="text-grey-dark">english - russian <br>
-           polish - russian <br>
-           bulgarian - russian <br>
-           german - russian
-        </p>
-      </div>
-      <div class="w-2/3">
-        <h3 class="text-white font-normal text-2xl">your benefits</h3>
-        <p class="text-grey-dark">high-quality translation <br>
-           quick and timely support from me <br>
-           strict confidentiality of your information <br>
-           getting the translation within the deadline you specify <br>
-           discounts and benefits for my long-term clients
-        </p>
+  <div class="w-1/2 bg-brown h-screen relative first-section">
+    <div class="absolute ml-8 mb-8 pin-b">
+      <h1 class="text-white text-7xl tracking-tight">Anna Schukina</h1>
+      <h1 class="text-white text-7xl tracking-tight">Translations</h1>
+      <h2 class="text-grey-darker font-normal">professional. transaltion. service</h2>
+      <div class="flex flex-wrap">
+        <div class="w-1/3">
+          <h3 class="text-white font-normal text-2xl">languages</h3>
+          <p class="text-grey-dark">english - russian <br>
+             polish - russian <br>
+             bulgarian - russian <br>
+             german - russian
+          </p>
+        </div>
+        <div class="w-2/3">
+          <h3 class="text-white font-normal text-2xl">your benefits</h3>
+          <p class="text-grey-dark">high-quality translation <br>
+             quick and timely support from me <br>
+             strict confidentiality of your information <br>
+             getting the translation within the deadline you specify <br>
+             discounts and benefits for my long-term clients
+          </p>
+        </div>
       </div>
     </div>
   </div>
